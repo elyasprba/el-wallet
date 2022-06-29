955def44bd01b00aa86eff9abe89bfe48a7681b0
elyasprba <elyaspoerba21@gmail.com>
2022-06-29 17:36:41 +0700

    update setup repo

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function Login() {
+   return (
+      <Layout title="About">
+         <h1>WELCOME LOGIN</h1>
+      </Layout>
+   );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+   return (
+      <>
+         <Layout title="Home">
+            <h1>WELCOME HOME</h1>
+         </Layout>
+      </>
+   );
+}
