2605578b93affbae465b4c98297d139a8240d06b
elyasprba <elyaspoerba21@gmail.com>
2022-07-02 16:22:43 +0700

    update get user data by id navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,30 @@
 import style from './navbar.module.css';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 export default function Navbar() {
+   const selectUserId = useSelector((state) => state.loginReducer.loginData.id);
+   const selectTokenId = useSelector((state) => state.loginReducer.loginData.token);
+   const [user, setUser] = useState('');
+
+   useEffect(() => {
+      const userId = async () => {
+         try {
+            const config = { headers: { Authorization: `Bearer ${selectTokenId}` } };
+            const result = await axios.get(`https://fazzpay.herokuapp.com/user/profile/${selectUserId}`, config);
+            setUser(result.data.data);
+            console.log(result);
+         } catch (error) {
+            console.log(error);
+         }
+      };
+      userId();
+   }, []);
+
+   console.log(user);
+
    return (
       <>
          <div>
@@ -14,8 +37,8 @@ export default function Navbar() {
                      <Image src={require('../../assets/img.png')} alt="img" width={40} height={40} />
                   </div>
                   <div className={style.nameInfo}>
-                     <p className={style.nameUser}>Robert Chandler</p>
-                     <p className={style.nameNumber}>+62 8139 3877 7946</p>
+                     <p className={style.nameUser}>{`${user.firstName} ${user.lastName}`}</p>
+                     <p className={style.nameNumber}>{`${user.noTelp}`}</p>
                   </div>
                   <div>
                      <Image src={require('../../assets/vector/bel.png')} alt="img" width={30} height={30} />
